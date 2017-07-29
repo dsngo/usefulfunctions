8ef1591fce447c63f466b01c2a13e6cd12e465e4
geart891 <daniel.dungngo@gmail.com>
2017-07-29 17:10:46 +0700

    first commit

---
+++ colorManipulation.js
@@ -0,0 +1,13 @@
+// function invertColorExplain(color) {
+//   const bitwiseVal = 0xFFFFFF ^ parseInt(color.substring(1),16);
+//   const convert = `000000${bitwiseVal.toString(16)}`
+//   const colorString = convert.slice(-6);
+//   return `#${colorString}`
+// }
+// const color = `#00acbd`
+// console.log(invertColor(color)); // #ff5342
+
+function invertColor(color) {
+  const bitwiseVal = 0xFFFFFF ^ parseInt(color.substring(1),16);
+  return `#${`000000${bitwiseVal.toString(16)}`.slice(-6)}`
+}
