464c3cfe8a4a687d03642fa5a7d006a74df9d771
geart891 <daniel.dungngo@gmail.com>
2017-07-31 17:05:09 +0700

    add test

---
+++ test.js
@@ -0,0 +1,11 @@
+const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
+const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=paris');
+
+Promise
+  .all([postsPromise, streetCarsPromise])
+  .then(responses => {
+    return responses.map(res => res.json())
+  })
+  .then(responses => {
+    console.log(responses);
+  });
